c7a3b978fdd6277313ea3deec4ff7ddeb134b896
gab <114111654+gabalbcos@users.noreply.github.com>
2023-09-22 21:54:20 -0300

    add filter functionality

--- gab-gym/src/pages/exercises/Filters/Filters.tsx
+++ gab-gym/src/pages/exercises/Filters/Filters.tsx
@@ -1,5 +1,6 @@
 import filters from './filters.json';
 import styles from './Filters.module.scss';
+import classNames from 'classnames';
 
 type IOption = typeof filters[0];
 
@@ -11,12 +12,16 @@ interface Props {
 
 export default function Filters({filter, setFilter}: Props) {
     function selectFilter(option: IOption) {
+        if (filter === option.id) return setFilter(null);
         return setFilter(option.id);
     }
     return(
         <div className={styles.filters}>
             {filters.map((option) => ( 
-                <button className={`${styles.filters__filter} ${filter === option.id ? styles["filters__filter--active"]: ""}`} key={option.id} onClick={() => selectFilter(option)}>
+                <button className={classNames({
+                    [styles.filters__filter]: true,
+                    [styles['filters__filter--active']]: filter === option.id
+                })} key={option.id} onClick={() => selectFilter(option)}>
                     {option.label}
                 </button>
 
