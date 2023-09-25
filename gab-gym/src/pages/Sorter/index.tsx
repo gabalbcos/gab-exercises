f040cbfa8551af6e560ae1cfcf6c5af0747de527
gab <114111654+gabalbcos@users.noreply.github.com>
2023-09-24 23:40:19 -0300

    add responsive style to Sorter

--- gab-gym/src/pages/Sorter/index.tsx
+++ gab-gym/src/pages/Sorter/index.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import styles from "./Sorter.module.scss"
 import options from "./options.json"
+import classNames from 'classnames';
 
 
 export default function Sorter() {
+    const [open, setOpen] = useState(false);
     return(
-        <button className={styles.sorter}>
+        <button 
+                onClick={() => setOpen(!open)}
+                className={styles.sorter}
+                onBlur={() => setOpen(false)}
+                >
+
         <span>Sort by:</span>
-        <div className={styles.sorter__options}>
+        <div className={classNames({
+            [styles.sorter__options]: true,
+            [styles["sorter__options--active"]]: open
+        })}>
             {options.map(option => (
-            <div className={styles.sorter} key={option.value}>
+            <div className={styles.sorter__options} key={option.value}>
                 {option.name}
             </div>
         ))}
