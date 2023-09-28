ea36138cf1cb15aacf58969a5f98051ba2d8a087
gab <114111654+gabalbcos@users.noreply.github.com>
2023-09-27 21:43:22 -0300

    import items to Item component

--- gab-gym/src/pages/Itens/Item/index.tsx
+++ gab-gym/src/pages/Itens/Item/index.tsx
@@ -1,25 +1,33 @@
 import styles from './Item.module.scss'
+import exercises from '../itens.json'
 
-export default function Item() {
+type Props = typeof exercises[0];
+
+export default function Item(props: Props) {
+    const { title, description, primaryMuscle, category, difficulty, photo, reps} = props;
     return(
         <div className={styles.item}>
             <div className={styles.item__image}>
-                <img src={} alt="exercise photo"/>
+                <img src={photo} alt={title}/>
             </div>
             <div className={styles.item__description}>
-                <div className={styles.item__titulo}>
-                    <h2 className=']'></h2>
-                    <p></p>
+                <div className={styles.item__title}>
+                    <h2 >{title}</h2>
+                    <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
                     <div className={styles.item__type}>
-
+                        {category.label}
                     </div>
-                    <div   className={styles.item__portion}>
-                        
+                    <div className={styles.item__muscleGroup}>
+                        {primaryMuscle}
+                    </div>
+                    
+                    <div   className={styles.item__difficulty}>
+                        {difficulty}
                     </div>
                     <div className={styles.item__value}> 
-                         
+                         {reps}
                     </div>
 
                 </div>
